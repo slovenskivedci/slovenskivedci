ecfc168b9d97e0ac2ffff066a4627f50a8bad699
Martin Takac <martin.taki@gmail.com>
2021-09-15 23:53:04 +0400

    add countries and insitutions pages

---
+++ clean.py
@@ -0,0 +1,21 @@
+import yaml
+
+import glob
+countries={}
+institutions={}
+alllst=[]
+
+from urllib.parse import unquote
+
+ 
+for y in glob.glob("./people/*.yaml"):
+	with open(y) as f:
+		dic = yaml.safe_load(f)
+		
+			
+	
+#	with open(y, 'w') as file:
+#		documents = yaml.dump(dic, file)
+	 
+
+
--- process.py
+++ process.py
@@ -1,9 +1,22 @@
 import yaml
+import os
 
 import glob
 countries={}
 institutions={}
 alllst=[]
+
+
+os.system("rm country_*")
+os.system("rm affiliation_*")
+os.system("rm _data/*yaml")
+
+
+import unicodedata
+
+def repl(text):
+	return unicodedata.normalize('NFD', text).encode('ascii', 'ignore').decode('UTF-8').replace(",","_")
+
 for y in glob.glob("./people/*.yaml"):
 	with open(y) as f:
 		dic = yaml.safe_load(f)
@@ -11,22 +24,33 @@ for y in glob.glob("./people/*.yaml"):
 			countries[dic['country']] = []
 		if dic['affiliation'] not in institutions:
 			institutions[dic['affiliation']] = []
-		dic["countryurl"]=dic["country"].replace(" ","_")
-		dic["affiliationurl"]=dic["affiliation"].replace(" ","_")
+		dic["countryurl"]=repl(dic["country"].replace(" ","_"))
+		dic["affiliationurl"]=repl(dic["affiliation"].replace(" ","_"))
 		
 		institutions[dic['affiliation']].append(dic)			
 		countries[dic['country']].append(dic)
 		alllst.append(dic)
 		
-		
+	
 for k in countries:
 	countries[k] = sorted(countries[k],key= lambda e: -int(e['hindex']))
-	with open(r'_data/country_%s.yaml'%k.replace(" ","_"), 'w') as file:
-			documents = yaml.dump(countries[k], file)		
+	countrycode = repl(k.replace(" ","_"))
+	with open(r'_data/country_%s.yaml'%countrycode, 'w') as file:
+			documents = yaml.dump(countries[k], file)	
+
+	os.system("sed 's/DATAFILE/country_%s/g' template.style > country_%s.html"%(countrycode,countrycode))
+	
+			
+			
+				
 for k in institutions:
 	institutions[k] = sorted(institutions[k],key= lambda e: -int(e['hindex']))		
-	with open(r'_data/institution_%s.yaml'%k.replace(" ","_"), 'w') as file:
+	affcode = repl(k.replace(" ","_"))
+	print(affcode)
+	 
+	with open(r'_data/institution_%s.yaml'%affcode, 'w') as file:
 			documents = yaml.dump(institutions[k], file)		
+	os.system("sed 's/DATAFILE/institution_%s/g' template.style > affiliation_%s.html"%(affcode,affcode))
 
 
 
