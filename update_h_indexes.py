8216ce81951adce2a43de7e1ed9183fd40833e57
Martin Takac <martin.taki@gmail.com>
2021-09-16 19:01:08 +0400

    Add last_update

--- update_h_indexes.py
+++ update_h_indexes.py
@@ -4,12 +4,18 @@ import os
 
 import glob
 
+for y in glob.glob("./config/conf.yaml"):
+	with open(y) as f: conf = yaml.safe_load(f)
+
+
+people=[]
 for y in glob.glob("./people/*.yaml"):
 	with open(y) as f:
 		dic = yaml.safe_load(f)
-		print(dic)
-		
-conf = {"username":"TODO","password":"password"}
+		if "last_update" not in dic:
+			dic["last_update"]="2020-12-31"
+			with open(y, 'w') as file:
+			   documents = yaml.dump(dic, file)
 
-with open(r'config/conf.yaml.template', 'w') as file:
-		documents = yaml.dump(conf, file)
+
+ 
