431996f4de48763d53613d4e89f527ae0448e2fc
Martin Takac <martin.taki@gmail.com>
2022-04-01 17:20:44 +0400

    cleanap

--- clean.py
+++ clean.py
@@ -9,13 +9,21 @@ from urllib.parse import unquote
 
  
 for y in glob.glob("./people/*.yaml"):
+	person = y.split("/")[2]
+	name = person.split("_")[0]
+	surname = person.split("_")[1].split(".")[0]
+	
+	newname = name.upper()[0]+name.lower()[1:]+"_"+surname.upper()[0]+surname.lower()[1:]+".yaml"
+	print(newname)
+	 
 	with open(y) as f:
 		dic = yaml.safe_load(f)
-		if "cited_by" in dic:
-			del dic["cited_by"]
+	with open(y, 'w') as file:
+		documents = yaml.dump([], file)
+		
 
-			with open(y, 'w') as file:
-				documents = yaml.dump(dic, file)
+	with open("./people/"+newname, 'w') as file:
+	   documents = yaml.dump(dic, file)
 	 
 
 
