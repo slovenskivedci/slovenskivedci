877f92cd6701b16ff7256e6edb5924aa9f66e5b0
Martin Takac <martin.taki@gmail.com>
2022-04-02 13:22:44 +0400

    fix order of elements

--- clean.py
+++ clean.py
@@ -1,5 +1,5 @@
-import yaml
 
+import oyaml as yaml
 import glob
 countries={}
 institutions={}
@@ -9,21 +9,15 @@ from urllib.parse import unquote
 
  
 for y in glob.glob("./people/*.yaml"):
-	person = y.split("/")[2]
-	name = person.split("_")[0]
-	surname = person.split("_")[1].split(".")[0]
-	
-	newname = name.upper()[0]+name.lower()[1:]+"_"+surname.upper()[0]+surname.lower()[1:]+".yaml"
-	print(newname)
-	 
+	print(y) 
 	with open(y) as f:
 		dic = yaml.safe_load(f)
-	with open(y, 'w') as file:
-		documents = yaml.dump([], file)
 		
-
-	with open("./people/"+newname, 'w') as file:
-	   documents = yaml.dump(dic, file)
+#	with open("/tmp/test.yaml", 'w') as file:
+#	   documents = yaml.dump(dic, file)
+#	afsadf
+#	with open("./people/"+newname, 'w') as file:
+#	   documents = yaml.dump(dic, file)
 	 
 
 
