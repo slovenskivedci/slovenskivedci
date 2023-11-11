04649b79275f6617b2f4bea5a86071a9eda0cda7
Martin Takac <martin.taki@gmail.com>
2023-11-11 15:21:27 +0400

    cron: update H-values

--- update_h_indexes.py
+++ update_h_indexes.py
@@ -20,7 +20,7 @@ for y in glob.glob("./config/conf.yaml"):
 
 
 import numpy as np
-KEY = np.random.randint(10)
+KEY = np.random.randint(9)
  
 payload = {'api_key': conf['apikey%d'%KEY],
   'url': 'URL'}
